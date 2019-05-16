6064302a9a7069c88212b527884be089da0b0a7a
htkang <362542580@qq.com>
2019-05-17 00:19:01 +0800

    vuex-localStorage+try-catch

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,23 @@ import Vuex from 'Vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '成都'
+try {
+  if (localStorage.city) {
+    defaultCity = localStorage.city
+  }
+} catch (e) {}
+
 export default new Vuex.Store({
   state: {
-    city: '成都'
+    city: defaultCity
   },
   mutations: {
     changeCity (state, city) {
       state.city = city
+      try {
+        localStorage.city = city
+      } catch (e) {}
     }
   }
 })
